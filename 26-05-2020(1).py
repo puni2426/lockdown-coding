4a21fa02019d9e4f471ba2aa78218a5be62ef5d0
puni2426 <53472959+puni2426@users.noreply.github.com>
2020-05-26 18:34:00 +0530

    Create 26-05-2020(1).py

---
+++ 26-05-2020(1).py
@@ -0,0 +1,7 @@
+/*QUESTION
+ Python Program to Print all Integers that Aren’t Divisible by Either 2 or 3 and Lie between 1 and 50*/
+ 
+ 
+ for i in range(0,51):
+    if(i%2!=0 and i%3!=0):
+        print(i)
