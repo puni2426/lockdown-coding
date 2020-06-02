7ac11eeb1961a90e254835c022d21f43d71836c3
puni2426 <53472959+puni2426@users.noreply.github.com>
2020-06-02 18:26:38 +0530

    Update 2-06-20.c

--- 2-06-20.c
+++ 2-06-20.c
@@ -1,21 +1,27 @@
 /*Inversion count*/
+
+
 #include <stdio.h> 
 int getInvCount(int arr[], int n) 
 { 
-    int inv_count = 0; 
-    for (int i = 0; i < n - 1; i++) 
-        for (int j = i + 1; j < n; j++) 
-            if (arr[i] > arr[j]) 
-                inv_count++; 
-  
-    return inv_count; 
+	int inv_count = 0; 
+	for (int i = 0; i < n - 1; i++) 
+		for (int j = i + 1; j < n; j++) 
+			if (arr[i] > arr[j]) 
+				inv_count++; 
+
+	return inv_count; 
 } 
-  
 
 int main() 
-{   
-    int arr[] = { 1, 20, 6, 4, 5 }; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
-    printf(" Number of inversions are %d \n", getInvCount(arr, n)); 
-    return 0; 
-} 
+{ 
+	int arr[100]; 
+	int n,t;
+	printf("enter n value\n");
+	scanf("%d",&n);
+	printf("enter array elements\n");
+	for(int i=0;i<n;i++)
+	scanf("%d",&arr[i]);
+	printf(" Number of inversions are %d \n", getInvCount(arr, n)); 
+	return 0; 
+}
