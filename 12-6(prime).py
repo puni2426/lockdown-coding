913de2d2feb7dfd3ea6d07b89eece1e69b65e3c5
puni2426 <53472959+puni2426@users.noreply.github.com>
2020-06-12 18:02:17 +0530

    Create 12-6(prime).py

---
+++ 12-6(prime).py
@@ -0,0 +1,15 @@
+
+/*Given two positive integer start and end. The task is to write a Python program to print all Prime numbers in an Interval.  (#115)
+*/
+  
+start = 11
+end = 50
+  
+for val in range(start, end + 1): 
+    if val > 1: 
+        for n in range(2, val//2 + 2): 
+            if (val % n) == 0: 
+                break
+            else: 
+                if n == val//2 + 1: 
+                    print(val) 
