4ce39850df0a67bf0eabfd35d26f917a35c42918
puni2426 <53472959+puni2426@users.noreply.github.com>
2020-07-13 19:22:38 +0530

    Create 13-7(dublicate string).java

---
+++ 13-7(dublicate string).java
@@ -0,0 +1,20 @@
+/*Write a Java Program to find the duplicate characters in a string.
+*/
+public class DuplStr {
+ public static void main(String argu[]) {
+
+  String str = "puneeth";
+  int cnt = 0;
+  char[] inp = str.toCharArray();
+  System.out.println("Duplicate Characters are:");
+  for (int i = 0; i < str.length(); i++) {
+   for (int j = i + 1; j < str.length(); j++) {
+    if (inp[i] == inp[j]) {
+     System.out.println(inp[j]);
+     cnt++;
+     break;
+    }
+   }
+  }
+ }
+}
